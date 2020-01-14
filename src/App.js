4e2b8953e4eae9859ee1b21b5365f5151f2c3cc4
Michael Maramba <mpmaramb@calpoly.edu>
2020-01-13 20:58:04 -0800

    look and feel 1/13

--- src/App.js
+++ src/App.js
@@ -1,24 +1,76 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Component1 from './components/Component1.js';
+import Component2 from './components/Component2.js';
+import { 
+  Button, 
+  Row,
+  Col,
+  Icon,
+  Layout
+} from 'antd';
+import {
+  HashRouter,
+  Route,
+  Link,
+  Switch
+} from "react-router-dom";
+
+const { Header, Footer, Sider, Content } = Layout;
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <HashRouter>
+        <Row justify="end">
+          <Col span={2} className="leftCol">
+            <div className="leftIcon">
+              <Link to="/">
+                <Icon type="home" />
+              </Link>
+            </div>
+            <div className="leftIcon">
+              <Link to="/test">
+                <Icon type="book" />
+              </Link>
+            </div>
+            <div className="leftIcon">
+              <Link to="/test">
+                <Icon type="calendar" />
+              </Link>
+            </div>
+            <div className="leftIcon">
+              <Link to="/test">
+                <Icon type="check-circle" />
+              </Link>
+            </div>
+          </Col>
+          <Col span={8} className="midCol">
+            <Layout className="layout">
+              <Header className="midColHeader">
+                <div>
+                  Page Header
+                </div>
+              </Header>
+              <Content className="midColContent">
+                <div>
+                  <div>Something</div>
+                  <div>Something else</div>
+                  <div>Some other thing</div>
+                  <div>Last thing</div>
+                </div>
+              </Content>
+            </Layout>      
+          </Col>
+          <Col span={14} className="rightCol">
+            <Switch>
+              <Route path="/" exact component={Component1} />
+              <Route path="/test" component={Component2} />
+            </Switch>
+          </Col>
+        </Row>
+      </HashRouter>
     </div>
   );
 }
